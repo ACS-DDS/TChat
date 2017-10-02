ad678a1a66805eabeed91cb9552a1a9d9e4fbf2d
Kasai <kasaidot@gmail.com>
2017-10-02 11:09:35 +0200

    complete ea6a01b
    - remove the old url system

--- register.php
+++ register.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
+
 if(isset($_POST["nom"])) : 
 	if(!isset($_POST["nom"]) || $_POST["nom"] == "" || !isset($_POST["prenom"]) || $_POST["prenom"] == "" || !isset($_POST["password"]) || $_POST["password"] == "" || !isset($_POST["mail"]) || $_POST["mail"] == "" || !isset($_POST["img"]) || $_POST["img"] == "" || !isset($_POST["pseudo"]) || $_POST["pseudo"] == ""){
 		$errors[] = "Merci de renseigner tous les champs !";
 		$_SESSION["errors"] = $errors;
-		header("Location: http://corentinp.dijon.codeur.online/TChat/login");
+		header("Location: ./TChat/login");
 		exit;
 	}
 
@@ -17,11 +18,11 @@ if(isset($_POST["nom"])) :
 		$errors[] = "Vous avez bien été enregistré en tant que <b>" . $_POST["nom"] . "</b>, veuillez vous connecter pour acceder au site web !";
 		$_SESSION["errors"] = $errors;
 
-		header("Location: http://corentinp.dijon.codeur.online/TChat/login");
+		header("Location: ./TChat/login");
 		exit;
 	}
 
 	$errors[] = "Saisie incorrecte !";
 	$_SESSION["errors"] = $errors;
-	header("Location: http://corentinp.dijon.codeur.online/TChat/login");
+	header("Location: ./TChat/login");
 endif;?>
