7ac4f300839a391dbf359f1fbaff74daed00c3c7
Corentin PERROT <cp425404@ghostiem.iem>
2017-10-04 13:21:21 +0200

    Edit a bit the backend. removed the old links & old files

--- .data/php/controle.php
+++ .data/php/controle.php
@@ -1,12 +1,13 @@
 <?php
+
 session_start();
 
-/*if($_SERVER['HTTP_REFERER'] != "http://corentinp.dijon.codeur.online/TChat/login"){
-	header("Location: http://corentinp.dijon.codeur.online/TChat/hotlink");
+/* if($_SERVER['HTTP_REFERER'] != "./TChat/login"){
+	header("Location: ./TChat/hotlink");
 	exit;
-}*/
+} */
 if(!isset($_SESSION["username"])) : 
-	header("Location: http://corentinp.dijon.codeur.online/TChat/login");
+	header("Location: ./TChat/login");
 	exit;
 endif;
 ?>
